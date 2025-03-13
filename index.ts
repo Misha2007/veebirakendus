95279b51583e4add0f6f50388328b33d3607de33
Misha2007 <mishadrogovoz@gmail.com>
2025-03-13 14:39:26 +0200

    Kontrolleri koostamine

--- index.ts
+++ index.ts
@@ -1,10 +1,29 @@
+import mongoose from "mongoose";
 import express, { Express, Request, Response } from "express";
+import articleController from "./controllers/articleController";
+
+mongoose.connect(
+  "mongodb+srv://mykhailodrogovoz:ZmGFiBmmuXjMFC79@cluster0.fa4uj.mongodb.net/"
+);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app: Express = express();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+app.use("/", articleController);
 
 app.listen(3000, () => {
   console.log(`[server]: Server is running at http://localhost:3000`);
